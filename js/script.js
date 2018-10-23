589d680d33eb96d47ceaaae6a01915a52bdb306f
KurtSwinehart <kurtswinehart@gmail.com>
2018-10-22 23:01:19 -0400

    making progress

--- js/script.js
+++ js/script.js
@@ -87,3 +87,29 @@ function myFunction() {
         x.style.display = "none";
     }
 }
+
+// Get the modal - REMOVE AFTER GRADING
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal- REMOVE AFTER GRADING
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal- REMOVE AFTER GRADING
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal - REMOVE AFTER GRADING
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal- REMOVE AFTER GRADING
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it- REMOVE AFTER GRADING
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
