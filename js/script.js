1c5afb7bc3e595347025ba6055beaff0bbb69210
KurtSwinehart <kurtswinehart@gmail.com>
2018-10-24 01:04:06 -0400

    working on mq

--- js/script.js
+++ js/script.js
@@ -13,38 +13,47 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-// <-- remove button after project is graded -->
 function myFunction() {
     var x = document.getElementById("myNav");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    if (x.className === "topnav") {
+        x.className += " responsive";
     } else {
-        x.style.display = "none";
+        x.className = "topnav";
     }
 }
+
+// <-- remove button after project is graded -->
+// function myFunction() {
+//     var x = document.getElementById("myNav");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
 
 // Get the modal - REMOVE AFTER GRADING
-var modal = document.getElementById('myModal');
+// var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal- REMOVE AFTER GRADING
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal- REMOVE AFTER GRADING
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal - REMOVE AFTER GRADING
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal- REMOVE AFTER GRADING
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it- REMOVE AFTER GRADING
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
